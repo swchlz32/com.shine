3067df83c6585f667a976c9014e52e562f7a87bf
swchlz32 <128011688+swchlz32@users.noreply.github.com>
2023-03-17 16:33:12 +0800

    Update GitTest.java
    1

--- src/main/java/com/shine/GitTest.java
+++ src/main/java/com/shine/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("master commit");
         System.out.println("dev test");
         System.out.println("dev test");
+        System.out.println("dev pull test");
     }
 }
